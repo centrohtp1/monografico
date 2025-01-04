0403038ac20d5e07aa421c0a8dbdf04a19d6da69
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-04 13:17:37 -0400

    Update views.py

--- estudiantes/views.py
+++ estudiantes/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .models import Estudiante
 from .serializers import EstudianteSerializer
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework.parsers import JSONParser, FormParser
 class EstudianteViewSet(viewsets.ModelViewSet):
     queryset = Estudiante.objects.all()
     serializer_class = EstudianteSerializer

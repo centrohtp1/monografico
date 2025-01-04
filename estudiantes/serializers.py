91bfc299fc393ded804f798646621a0f211566a2
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-04 13:12:41 -0400

    Update serializers.py

--- estudiantes/serializers.py
+++ estudiantes/serializers.py
@@ -1,8 +1,10 @@
 # estudiantes/serializers.py
+ #  fields = '__all__'
 from rest_framework import serializers
 from .models import Estudiante
 
 class EstudianteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudiante
-        fields = '__all__'
+        exclude = ['matricula']
+     

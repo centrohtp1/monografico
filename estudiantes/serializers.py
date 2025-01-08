1d4d4a9500ae45d46ae8e44b05d5ff0315b915ac
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-08 00:13:20 -0400

    Update serializers.py

--- estudiantes/serializers.py
+++ estudiantes/serializers.py
@@ -1,10 +1,9 @@
-# estudiantes/serializers.py
- #  fields = '__all__'
 from rest_framework import serializers
 from .models import Estudiante
 
 class EstudianteSerializer(serializers.ModelSerializer):
+    matricula = serializers.ReadOnlyField()  # Esto hará que 'matricula' sea solo lectura
+    
     class Meta:
         model = Estudiante
-        exclude = ['matricula']
-     
+        fields = '__all__'  # Incluye todos los campos del modelo

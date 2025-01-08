013e0d6395bbda990eec16054d9955b8413a8fc6
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-08 11:36:36 -0400

    Update urls.py

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,1 +1,8 @@
+# estudiantes/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('reporte-estudiantes/', views.get_estudiantes_report, name='get_estudiantes_report'),
+]
 

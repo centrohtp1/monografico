e9dd4a72912ce647988d95dc4016bb84a5282772
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-08 10:56:55 -0400

    Update models.py

--- Cursos/models.py
+++ Cursos/models.py
@@ -1,14 +1,14 @@
-# cursos/models.py
 from datetime import timezone
 from django.db import models
 from estudiantes.models import Estudiante  # Importa desde la app correcta
 from profesores.models import Profesor    # Importa desde la app correcta
 from django.utils.timezone import now 
+
 class Curso(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)  # Campo único para el nombre del curso
     descripcion = models.TextField()
     profesores = models.ForeignKey(Profesor, on_delete=models.SET_NULL, null=True)
-    fecha_creacion = models.DateField(default=now  )
+    fecha_creacion = models.DateField(default=now)
 
     def __str__(self):
         return self.nombre

09176f984f4ef038be696142324f0444985cf3fe
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-09 21:08:28 -0400

    actuali

--- reportes/views.py
+++ reportes/views.py
@@ -1,6 +1,14 @@
 from django.http import JsonResponse
 from estudiantes.models import Estudiante
 import pandas as pd
+from django.http import JsonResponse
+from facturacion.models import Tarifa
+from django.http import JsonResponse
+from secciones.models import Seccion
+from profesores.models import Profesor
+from facturacion.models import Factura
+from Cursos.models import Curso
+
 
 def get_estudiantes_report(request):
     fecha_inicio = request.GET.get('fecha_inicio')
@@ -21,5 +29,97 @@ def get_estudiantes_report(request):
 
         # Retornar el JSON con los datos de los estudiantes
         return JsonResponse({'estudiantes': estudiantes_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Por favor proporciona las fechas de inicio y fin'}, status=400)
+
+def get_tarifas_report(request):
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    if fecha_inicio and fecha_fin:
+        try:
+            fecha_inicio = pd.to_datetime(fecha_inicio)
+            fecha_fin = pd.to_datetime(fecha_fin)
+        except ValueError:
+            return JsonResponse({'error': 'Formato de fecha incorrecto'}, status=400)
+
+        tarifas = Tarifa.objects.filter(fecha_creacion__range=(fecha_inicio, fecha_fin))
+        tarifas_data = list(tarifas.values('id', 'secciones_tarifa__nombre', 'precio', 'fecha_creacion'))
+
+        return JsonResponse({'tarifas': tarifas_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Por favor proporciona las fechas de inicio y fin'}, status=400)
+
+def get_facturas_report(request):
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    if fecha_inicio and fecha_fin:
+        try:
+            fecha_inicio = pd.to_datetime(fecha_inicio)
+            fecha_fin = pd.to_datetime(fecha_fin)
+        except ValueError:
+            return JsonResponse({'error': 'Formato de fecha incorrecto'}, status=400)
+
+        facturas = Factura.objects.filter(fecha_emision__range=(fecha_inicio, fecha_fin))
+        facturas_data = list(facturas.values('id', 'estudiante__nombre', 'total', 'fecha_emision', 'mes_correspondiente', 'pagado'))
+
+        return JsonResponse({'facturas': facturas_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Por favor proporciona las fechas de inicio y fin'}, status=400)
+
+def get_profesores_report(request):
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    if fecha_inicio and fecha_fin:
+        try:
+            fecha_inicio = pd.to_datetime(fecha_inicio)
+            fecha_fin = pd.to_datetime(fecha_fin)
+        except ValueError:
+            return JsonResponse({'error': 'Formato de fecha incorrecto'}, status=400)
+
+        profesores = Profesor.objects.filter(fecha_contratacion__range=(fecha_inicio, fecha_fin))
+        profesores_data = list(profesores.values('id', 'nombre', 'apellido', 'especialidad', 'fecha_contratacion'))
+
+        return JsonResponse({'profesores': profesores_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Por favor proporciona las fechas de inicio y fin'}, status=400)
+
+
+def get_cursos_report(request):
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    if fecha_inicio and fecha_fin:
+        try:
+            fecha_inicio = pd.to_datetime(fecha_inicio)
+            fecha_fin = pd.to_datetime(fecha_fin)
+        except ValueError:
+            return JsonResponse({'error': 'Formato de fecha incorrecto'}, status=400)
+
+        cursos = Curso.objects.filter(fecha_creacion__range=(fecha_inicio, fecha_fin))
+        cursos_data = list(cursos.values('id', 'nombre', 'descripcion', 'fecha_creacion'))
+
+        return JsonResponse({'cursos': cursos_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Por favor proporciona las fechas de inicio y fin'}, status=400)
+
+
+def get_secciones_report(request):
+    fecha_inicio = request.GET.get('fecha_inicio')
+    fecha_fin = request.GET.get('fecha_fin')
+
+    if fecha_inicio and fecha_fin:
+        try:
+            fecha_inicio = pd.to_datetime(fecha_inicio)
+            fecha_fin = pd.to_datetime(fecha_fin)
+        except ValueError:
+            return JsonResponse({'error': 'Formato de fecha incorrecto'}, status=400)
+
+        secciones = Seccion.objects.filter(fecha_inicio__range=(fecha_inicio, fecha_fin))
+        secciones_data = list(secciones.values('id', 'nombre', 'curso__nombre', 'fecha_inicio', 'fecha_termino'))
+
+        return JsonResponse({'secciones': secciones_data}, status=200)
     else:
         return JsonResponse({'error': 'Por favor proporciona las fechas de inicio y fin'}, status=400)

0d465d6bfa1f0cd4b4b1d36aa1d582a95e1e86de
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-09 21:46:48 -0400

    asas

--- reportes/urls.py
+++ reportes/urls.py
@@ -5,19 +5,19 @@ from . import views
 urlpatterns = [
     path('reporte-estudiantes/', views.get_estudiantes_report, name='get_estudiantes_report'),
       # Reporte de Secciones
-    path('reporte/secciones/', views.get_secciones_report, name='reporte_secciones'),
+    path('reporte-secciones/', views.get_secciones_report, name='reporte_secciones'),
 
     # Reporte de Cursos
-    path('reporte/cursos/', views.get_cursos_report, name='reporte_cursos'),
+    path('reporte-cursos/', views.get_cursos_report, name='reporte_cursos'),
 
     # Reporte de Profesores
-    path('reporte/profesores/', views.get_profesores_report, name='reporte_profesores'),
+    path('reporte-profesores/', views.get_profesores_report, name='reporte_profesores'),
 
     # Reporte de Facturas
-    path('reporte/facturas/', views.get_facturas_report, name='reporte_facturas'),
+    path('reporte-facturas/', views.get_facturas_report, name='reporte_facturas'),
 
     # Reporte de Tarifas
-    path('reporte/tarifas/', views.get_tarifas_report, name='reporte_tarifas'),
+    path('reporte-tarifas/', views.get_tarifas_report, name='reporte_tarifas'),
 ]
 
 

c9278ce7218270c2450c09b2625467663d13fc57
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-13 19:32:31 -0400

    13 de enero

--- anio_escolar/models.py
+++ anio_escolar/models.py
@@ -1,10 +1,10 @@
 # anio_escolar/models.py
 from django.db import models
-
+from django.utils import timezone
 class AnioEscolar(models.Model):
     nombre = models.CharField(max_length=10, unique=True, verbose_name="Año Escolar")  # Ej: 2024-2025
     activo = models.BooleanField(default=True, verbose_name="Activo")  # Para marcar el año escolar activo
-
+    fecha_ingreso = models.DateField(default=timezone.now) 
     def __str__(self):
         return self.nombre
 

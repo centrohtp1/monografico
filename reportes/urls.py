b5787d7111a0109d48445180321ab243b28ddb31
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-13 21:13:26 -0400

    1234

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,6 +1,6 @@
 # estudiantes/urls.py
 from django.urls import path
-from . import views
+from reportes import views
 
 urlpatterns = [
     path('reporte-estudiantes/', views.get_estudiantes_report, name='get_estudiantes_report'),

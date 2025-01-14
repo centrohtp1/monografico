9988c437aa3a18901bf832e28dffcaad39291f5f
centro htp <193030872+centrohtp1@users.noreply.github.com>
2025-01-13 22:12:38 -0400

    12313131313

--- reportes/urls.py
+++ reportes/urls.py
@@ -4,7 +4,7 @@ from reportes import views
 
 urlpatterns = [
     path('reporte-estudiantes/', views.get_estudiantes_report, name='get_estudiantes_report'),
-    path('reportes-anioescolar/', views.obtener_anos_escolares, name='get_anioescolar_report'),
+    path('reportes-anioescolar/', views.get_anos_escolares, name='get_anos_escolares'),
 
 ]
 

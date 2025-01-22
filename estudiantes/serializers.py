85488a996ca0630f51357e39627271e2f16811e6
centrohtp1 <centrocapacitacionhtm@gmail.com.com>
2025-01-21 21:49:15 -0400

    hay caramba

--- estudiantes/serializers.py
+++ estudiantes/serializers.py
@@ -3,26 +3,31 @@ from rest_framework import serializers
 from .models import Estudiante
 import re
 
+
 class EstudianteSerializer(serializers.ModelSerializer):
     matricula = serializers.ReadOnlyField()  # Esto hará que 'matricula' sea solo lectura
     
     # Validación personalizada para correo único
     def validate_email(self, value):
-        # Verificar si el correo electrónico ya existe
-        if Estudiante.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Este correo electrónico ya está registrado.")
+        # Obtener el estudiante que se está actualizando, si hay uno
+        estudiante = self.instance
+        
+        # Si el correo electrónico ha cambiado, se realiza la validación
+        if estudiante and value != estudiante.email:
+            # Verificar si el correo electrónico ya existe en otro estudiante
+            if Estudiante.objects.filter(email=value).exists():
+                raise serializers.ValidationError("Este correo electrónico ya está registrado.")
+        
         return value
 
-   # Validación personalizada para el campo nombre (letras, acentos y espacios)
+    # Validación personalizada para el campo nombre (letras, acentos y espacios)
     def validate_nombre(self, value):
-        # Expresión regular que permite letras, acentos y espacios
         if not re.match(r'^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$', value):
             raise serializers.ValidationError("El nombre solo puede contener letras, incluyendo acentos y espacios.")
         return value
 
     # Validación personalizada para el campo apellido (letras, acentos y espacios)
     def validate_apellido(self, value):
-        # Expresión regular que permite letras, acentos y espacios
         if not re.match(r'^[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ\s]+$', value):
             raise serializers.ValidationError("El apellido solo puede contener letras, incluyendo acentos y espacios.")
         return value
